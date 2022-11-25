626e0b7b0d0e1450d088efed66b9a08bfcb4ccfb
Joel Samuel <joel.sr1024@gmail.com>
2022-11-25 17:41:31 +0530

    Fix: header

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -8,7 +8,7 @@ import "./index.css"
 interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options }) => {
-  const {seconds} = options
+  const {seconds, text} = options
   const [progress, setProgress] = useState<number>(0)
   useEffect(() => {
     const timer = setInterval(() => {
@@ -22,7 +22,7 @@ export const SimplePanel: React.FC<Props> = ({ options }) => {
   return (
    <div className={`bg-[${theme.colors.bg2}] w-full h-full flex flex-col justify-evenly items-center p-4 rounded`}>
    <div className='flex justify-between w-full'>
-    <p className={`text-4xl text-[${theme.colors.textStrong}]`}> Time to load the next shot </p>
+    <p className={`text-4xl text-[${theme.colors.textStrong}]`}> {text} </p>
     <p className={`text-[${theme.colors.textWeak}]`}> {seconds - progress} sec remaining </p>  
    </div>
     <ProgressBar height="12px" customLabel=" " baseBgColor={theme.colors.bg2} bgColor={theme.colors.bgBlue1} maxCompleted={seconds} completed={progress} className="w-full" />
