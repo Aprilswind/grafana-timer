2c35a56b5fb633422e4f13fc8575d57aa176b654
Joel Samuel <joel.sr1024@gmail.com>
2022-11-26 22:16:26 +0530

    Feat: integrated mqtt

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,31 +1,60 @@
-import { PanelProps } from '@grafana/data';
+import { getFieldDisplayValues, PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import React, {useEffect, useState} from 'react';
-import { useTheme } from '@grafana/ui';
+import { useTheme2 as useTheme } from '@grafana/ui';
 import ProgressBar from "@ramonak/react-progress-bar";
 import "./index.css"
 
 interface Props extends PanelProps<SimpleOptions> {}
 
-export const SimplePanel: React.FC<Props> = ({ options }) => {
+export const SimplePanel: React.FC<Props> = ({ options, width, height, replaceVariables, fieldConfig, data }) => {
   const {seconds, text} = options
   const [progress, setProgress] = useState<number>(0)
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((p: number) => p + 1)
-    }, 1000)
-    setTimeout(() => clearInterval(timer), seconds * 1000)
-    return () => clearInterval(timer)
-  }, [seconds])
+  const [numeric, setNumeric] = useState<number>(0)
   const theme = useTheme();
-  console.log(theme)
+  console.log("rerender")
+  useEffect(() => {
+    try {
+      const meta = getFieldDisplayValues({
+        reduceOptions: { calcs: ['lastNotNull'] },
+          data: data.series,
+          replaceVariables,
+          fieldConfig,
+          theme
+      })
+      console.log(meta)
+      if (meta && meta.length) {
+        const {display} = meta[0]
+        setNumeric(display.numeric)
+      }
+    } catch (e) {
+      console.log("error", e) 
+    }
+  }, [])
+  useEffect(() => {
+    if (numeric) {
+      const timer = setInterval(() => {
+        setProgress((p: number) => p + 1)
+      }, 1000)
+      setTimeout(() => clearInterval(timer), seconds * 1000)
+      return () => clearInterval(timer)
+    } else {
+      console.log(numeric)
+      return () => {}
+    }
+  }, [seconds, numeric])
   return (
-   <div className={`bg-[${theme.colors.bg2}] w-full h-full flex flex-col justify-evenly items-center p-4 rounded`}>
+   <div 
+    style={{
+    backgroundColor: theme.colors.background.canvas,
+    width: width,
+    height: height
+   }} className={`flex flex-col justify-evenly items-center p-4 rounded-xl shadow-lg`}>
    <div className='flex justify-between w-full'>
-    <p className={`text-4xl text-[${theme.colors.textStrong}]`}> {text} </p>
-    <p className={`text-[${theme.colors.textWeak}]`}> {seconds - progress} sec remaining </p>  
+    <p className={`text-4xl text-[${theme.colors.text.primary}]`}> {numeric ? text : "Casting in progress"} </p>
+    { numeric ? <p className={`text-[${theme.colors.text.secondary}]`}> {seconds - progress} sec remaining </p> : <p> </p> } 
    </div>
-    <ProgressBar height="12px" customLabel=" " baseBgColor={theme.colors.bg2} bgColor={theme.colors.bgBlue1} maxCompleted={seconds} completed={progress} className="w-full" />
+    <ProgressBar height="12px" customLabel=" " baseBgColor={theme.colors.background.primary} bgColor={theme.colors.info.shade} maxCompleted={seconds} completed={numeric ? progress : seconds} className="w-full" />
    </div> 
   );
 };
